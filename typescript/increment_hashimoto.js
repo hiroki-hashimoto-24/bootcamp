d7da2baa350c872e5b9749930a132b22b07335cb
Hashimoto <hashimoto-h.jk@mb.meidensha.co.jp>
2024-09-04 12:43:02 +0900

    first commit

---
+++ typescript/increment_hashimoto.js
@@ -0,0 +1,7 @@
+//JavaScriptのプログラムの練習
+//作成者：hashimoto
+//作成日：2024.8.29
+function increment(num) {
+    return num + 1;
+}
+console.log(increment(999));
---
+++ typescript/increment_hashimoto.ts
@@ -0,0 +1,8 @@
+//JavaScriptのプログラムの練習
+//作成者：hashimoto
+//作成日：2024.8.29
+function increment(num: number) {
+  return num + 1;
+}
+
+console.log(increment(999));
